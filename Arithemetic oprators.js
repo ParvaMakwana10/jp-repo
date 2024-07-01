32c307348ab94041e875df57f318e17d3f8acc9a
jigar <jigarparmar9128@gmail.com>
2024-07-01 20:34:40 +0530

    new js file added

---
+++ Arithemetic oprators.js
@@ -0,0 +1,9 @@
+//Arithemetic oprator
+let a = 5;
+let b = 10;
+console.log("a=", a, "b=", b);
+console.log("a + b =", a + b);
+console.log("a -b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
